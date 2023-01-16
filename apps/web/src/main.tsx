936e9151c0d75d4c8bdd0f65e7837b2d47b761b7
Bohdanov Mikhail <bohdanov.mikhail@gmail.com>
2023-01-17 01:59:29 +0200

    Move old index.tsx file to new main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
+import { RouterProvider } from 'react-router-dom';
+import 'normalize.css';
 
-import App from './app/app';
+import routes from '@core/router';
+import store from '@core/store';
+import theme from '@core/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </ReduxProvider>
+  </StrictMode>,
 );
